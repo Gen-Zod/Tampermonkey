61d391ffce5dc0c8c1a32eda358d0cc146a9ac70
Gen-Zod <manuel.bissinger@gmail.com>
2015-10-09 21:42:39 +0200

    Update m-blass Rating.user.js

--- m-blass Rating.user.js
+++ m-blass Rating.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            m-blass Starbucks city mug star rating and direct mail
 // @namespace       
-// @version			0.2
+// @version			0.3
 // @description		Adds a star rating to Starbucks mugs, a user-collection-link-icon and the mail addresses get a mailto-Link
 // @homepageURL     https://github.com/Gen-Zod/Tampermonkey/blob/master/m-blass%20Rating.user.js
 // @supportURL      https://github.com/Gen-Zod/Tampermonkey/blob/master/m-blass%20Rating.user.js
@@ -21,14 +21,35 @@ String.prototype.repeat = function( num )
 }
 
 $(document).ready(function(){
-    // arrays of cities; array number is equal to commonness of a city mug (1 = uncommon to 4 = impossible to find)
-    var a_rateCities = new Array();
-    // list of cities are from http://mugs.m-blass.de/htf.php
-    a_rateCities[1] = ["Banff","Calgary","Canada","Edmonton","Montreal","Niagara Falls","Quebec","Toronto","Vancouver","Vancouver Island","Whistler","Oktoberfest I","Bali","Indonesia","Jakarta","Osaka","Tokyo","Kuala Lumpur","Penang","Sabah","Makati","Manila I","St. Petersburg I","Singapore I","Singapore II","Korea","Seoul","Alaska","Arizona","Atlanta","Austin","Boston","California","Charlotte","Chicago","Colorado","Dallas","Denver","Detroit","Florida","Fort Worth","Hamptons","Hawaii","Houston","Indianapolis","Lake Tahoe","Las Vegas","Los Angeles","Memphis","Miami","Nashville","New Mexico","New Orleans","New York","North Carolina","Ohio","Orange County","Orlando","Philadelphia","Phoenix","Pike Place Market","Pittsburgh","Portland","San Antonio","San Diego","San Francisco","Seattle II","St. Louis","Tampa","Twin Cities","Utah","Waikiki","Washington DC","Vietnam I"];
-    a_rateCities[2] = ["Bahrain I","England I","Sapporo","Nijmegen I","Qatar I","Cluj Napoca","Krasnodar I","Sochi I","Seville","Sharjah","Cleveland","Jacksonville","Sacramento","Seattle I"];
-    a_rateCities[3] = ["Zagreb","Corfu","Guanajuato I","Auckland","North Island","South Island","Lisbon","Portugal I"];
-    a_rateCities[4] = ["Cancun I","Chihuahua","Culiacan","Guadalajara","Monterrey","Puerto Vallarta I","Zacatecas"];
+    var $regex = new RegExp("^[A-Z]+\\s?[A-Z]*\\s*[A-Z]*\\s{1}\\-{1}\\s{1}(\\w*\\.*\\s?\\w*\\s*\\w*)$"); 
+    var a_rateCities = [];
     
+    //get the rare cities from http://mugs.m-blass.de/htf.php and store it in an array for later use
+    $.ajax({
+        url: "http://mugs.m-blass.de/htf.php", 
+        dataType: "text",
+        async: false,
+        success: function(data) {
+        var $page = $(data);
+        var $lists = $page.find('.dritte:lt(4)');
+        var $x = 1;
+        var $a_cities = [];
+        $lists.each(function() {
+            var $line = $(this).parent().nextAll().children('td[valign="top"]');
+            $a_cities[$x] = [];
+            $line.each(function() {
+                var $city_line = $(this).text();
+                if($city_line.match($regex)) { 
+                    var $city=$city_line.replace($regex, "\$1");
+                    $a_cities[$x].push($city);
+                }
+            });
+            $x++;
+        });    
+        a_rateCities = $a_cities.reverse(); // reverse the array from Impossible->Uncommon to Uncommon->Impossible for better star ranking
+    }
+    });
+
     var v_ratingString = ""; // rating string will be set inside the switch-case
     var v_ratingStar = '<img src="http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/16/Actions-rating-icon.png" width="12" height="12" />';
 
@@ -39,7 +60,7 @@ $(document).ready(function(){
     var v_mailBody = ""; // mail body will be set inside the loop, because it'll be contain the nickname of a user
     var v_nickname = ""; // nickname will be set inside the loop
     var v_tdString = ""; // String of a td-element will be set inside the loop
-    
+
     // iterate through all the td-Elements
     $('td').each(function() {
         v_tdString = $(this).text(); // get the string from a td-Element
@@ -52,11 +73,12 @@ $(document).ready(function(){
             // add an icon-link to the mug collection of the user next to the nickname
             $(this).prev().append('<a href="http://mugs.m-blass.de/collection.php?nickname=' + v_nickname + '" target="_blank" style="float: right; margin-right: 50%; background-color: transparent;" title="Collection of ' + v_nickname + '"><img src="http://icons.iconarchive.com/icons/custom-icon-design/mono-business-2/16/coffee-icon.png" width="12" height="12" /></a>');
         }
-        
+
         // iterate through the city arrays
-        for(i=1; i<a_rateCities.length; i++) {
+        for(i=0; i<4; i++) {
+            var k = i+1;
             // Translate ranking number to string (german and english)
-            switch(i) {
+            switch(k) {
                 case 1:
                     v_ratingString = "Selten / uncommon"
                     break;
@@ -77,7 +99,7 @@ $(document).ready(function(){
                 v_cityRegex = '^' + a_rateCities[i][j] + '\\s*$'; // RegEx to find city in text
                 if($(this).text().match(v_cityRegex)!=null) {
                     //Add star icons to city
-                    $(this).append(' <span title="' + v_ratingString + '">' + v_ratingStar.repeat(i) + '</span>');
+                    $(this).append(' <span title="' + v_ratingString + '">' + v_ratingStar.repeat(k) + '</span>');
                     break StopCitySearch;
                 }
             }
