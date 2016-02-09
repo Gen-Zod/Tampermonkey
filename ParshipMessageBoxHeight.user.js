e90cc5c4a2188f103de30f4c490541223e7db7d5
r4v3n50u1 <manuel.bissinger@gmail.com>
2016-02-09 13:52:49 +0100

    Create ParshipMessageBoxHeight.user.js

---
+++ ParshipMessageBoxHeight.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Parship Message Box Height
+// @namespace    
+// @version      0.1
+// @description  Increases the height of the message box at Parship
+// @author       Manuel Bissinger
+// @match        https://www.parship.de/messaging/writemessage/text?*
+// @homepageURL   https://github.com/r4v3n50u1/Tampermonkey/blob/master/ParshipMessageBoxHeight.user.js
+// @supportURL    https://github.com/r4v3n50u1/Tampermonkey/blob/master/ParshipMessageBoxHeight.user.js
+// @updateURL     https://github.com/r4v3n50u1/Tampermonkey/raw/master/ParshipMessageBoxHeight.user.js
+// @downloadURL   https://github.com/r4v3n50u1/Tampermonkey/raw/master/ParshipMessageBoxHeight.user.js
+// @grant         none
+// @require			http://code.jquery.com/jquery-latest.js
+// @run-at        document-end
+// ==/UserScript==
+
+$('#ps_writeMessage').height(35em);
