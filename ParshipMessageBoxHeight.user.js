3eaf4e75eee8a36c7266f320711acb9d3ee363d4
r4v3n50u1 <manuel.bissinger@gmail.com>
2016-02-11 21:34:52 +0100

    Update ParshipMessageBoxHeight.user.js
    removed jQuery lib since it broke the Parship scripts

--- ParshipMessageBoxHeight.user.js
+++ ParshipMessageBoxHeight.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Parship Message Box Height
 // @namespace    
-// @version      0.2
+// @version      0.3
 // @description  Increases the height of the message box at Parship
 // @author       Manuel Bissinger
 // @match        https://www.parship.de/messaging/writemessage/text?*
@@ -10,8 +10,28 @@
 // @updateURL     https://github.com/r4v3n50u1/Tampermonkey/raw/master/ParshipMessageBoxHeight.user.js
 // @downloadURL   https://github.com/r4v3n50u1/Tampermonkey/raw/master/ParshipMessageBoxHeight.user.js
 // @grant         none
-// @require			http://code.jquery.com/jquery-latest.js
 // @run-at        document-end
 // ==/UserScript==
 
-$('#ps_writeMessage').css('height', '35em');
+// since it doesn't work
+//$('#ps_writeMessage').css('height', '35em');
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+/* not needed as yet
+function addGlobalHTML(element,attr,val) {
+    var elem = document.getElementsByClassName(element)[0];
+    var att = document.createAttribute(attr);
+    att.value = val;
+    elem.setAttributeNode(att);
+}
+*/
+
+addGlobalStyle('#ps_writeMessage { height: 35em !important; }');
