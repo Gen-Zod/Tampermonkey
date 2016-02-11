d1f17b0b0a372490642ef7d09e03ee5172873da9
r4v3n50u1 <manuel.bissinger@gmail.com>
2016-02-11 08:43:13 +0100

    Update blog.fefe.de.user.js
    - Adding jQuery
    - Changing CSS-Styling to jQuery notation

--- blog.fefe.de.user.js
+++ blog.fefe.de.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         blog.fefe.de
-// @version      0.2
+// @version      0.3
 // @description  Dark Style for http://blog.fefe.de
 // @author       r4v3n50u1
 // @match        http*://blog.fefe.de/*
@@ -9,9 +9,29 @@
 // @supportURL   https://github.com/r4v3n50u1/Tampermonkey/blob/master/blog.fefe.de.user.js
 // @updateURL    https://github.com/r4v3n50u1/Tampermonkey/raw/master/blog.fefe.de.user.js
 // @downloadURL  https://github.com/r4v3n50u1/Tampermonkey/raw/master/blog.fefe.de.user.js
+// @require			http://code.jquery.com/jquery-latest.js
 // @run-at       document-end
 // ==/UserScript==
 
+$('body').css({
+    "background-color": "#222",
+    "font-family": "helvetica, sans-serif",
+    "font-size": "medium",
+    "color": "#eee",
+    "width": "960px",
+    "margin": "30px 25%"
+});
+$('a').css({
+    "color": "#eee"
+});
+$('h2>a').css({
+    "color": "#fff !important"
+});
+$('li>p').css({
+    "line-height": "1.5em",
+    "margin-left": "1.5em"
+});
+/*
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
@@ -27,3 +47,4 @@ addGlobalStyle('a { color: #eee; }');
 addGlobalStyle('h2>a { color: #fff !important; }');
 addGlobalStyle('li { line-height: 1.5em; background-color: #333; margin: 2px; border-radius: 5px; border: solid thin black; padding: 10px 15px; list-style: none; }');
 addGlobalStyle('li>p { line-height: 1.5em; margin-left: 1.5em; }');
+*/
